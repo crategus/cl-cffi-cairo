5e201614d4f8ffcd23647fa158194d80be258bdb
Crategus <kaiser@crategus.com>
2023-11-05 20:06:43 +0100

    Push Cairo versions on *features*

--- src/cairo.init.lisp
+++ src/cairo.init.lisp
@@ -33,23 +33,71 @@
        (t (:default "libcairo")))
 
   (cffi:use-foreign-library cairo)
+  ;; push the hostname on *features*
+  (pushnew (intern (string-upcase (machine-instance)) :keyword) *features*)
   (pushnew :cairo *features*))
 
 ;;; ----------------------------------------------------------------------------
 
-;; TODO: Implement this without using GLib.
+;;; Functions and macros to check the library version
 
-#+nil
-(glib-init::push-library-version-features cairo
-    (truncate (/ (cairo-version) 10000))
-    (- (truncate (/ (cairo-version) 100))
-       (* 100 (truncate (/ (cairo-version) 10000))))
-    1 16)
+(defmacro push-library-version-features (library-name
+                                         major-version-var
+                                         minor-version-var
+                                         &body versions)
+  `(eval-when (:load-toplevel :execute)
+     ,@(loop for (major minor) on versions by #'cddr
+             collect `(when (or (and (= ,major-version-var ,major)
+                                     (>= ,minor-version-var ,minor))
+                                (> ,major-version-var ,major))
+                        (pushnew ,(intern (format nil "~A-~A-~A"
+                                                  (string library-name)
+                                                  major minor)
+                                          (find-package :keyword))
+                                 *features*)))))
 
-#+nil
-(glib-init::require-library-version "Cairo" 1 16
-    (truncate (/ (cairo-version) 10000))
-    (- (truncate (/ (cairo-version) 100))
-       (* 100 (truncate (/ (cairo-version) 10000)))))
+(define-condition foreign-library-version-mismatch (error)
+  ((library :initarg :library :reader .library)
+   (minimum-version :initarg :minimum-version :reader .minimum-version)
+   (actual-version :initarg :actual-version :reader .actual-version))
+  (:report (lambda (c s)
+             (format s "Library ~A has too old version: it is ~A but required ~
+                        to be at least ~A"
+                       (.library c)
+                       (.actual-version c)
+                       (.minimum-version c)))))
+
+(defun require-library-version (library min-major-version
+                                        min-minor-version
+                                        major-version
+                                        minor-version)
+  (unless (or (> major-version min-major-version)
+              (and (= major-version min-major-version)
+                   (>= minor-version min-minor-version)))
+    (restart-case
+      (error 'foreign-library-version-mismatch
+             :library library
+             :minimum-version (format nil "~A.~A"
+                                      min-major-version min-minor-version)
+             :actual-version (format nil "~A.~A"
+                                     major-version minor-version))
+      (ignore () :report "Ignore version requirement" nil))))
+
+;;; ----------------------------------------------------------------------------
+
+(defparameter +cairo-version+ (cffi:foreign-funcall "cairo_version" :int))
+
+(push-library-version-features cairo
+    (truncate (/ +cairo-version+ 10000))
+    (- (truncate (/ +cairo-version+ 100))
+       (* 100 (truncate (/ +cairo-version+ 10000))))
+    1 16   ; Since 19-10-2018
+    1 18   ; Since 23-09-2023
+    1 20)
+
+(require-library-version "Cairo" 1 16
+    (truncate (/ +cairo-version+ 10000))
+    (- (truncate (/ +cairo-version+ 100))
+       (* 100 (truncate (/ +cairo-version+ 10000)))))
 
 ;;; --- End of file cairo.init.lisp --------------------------------------------

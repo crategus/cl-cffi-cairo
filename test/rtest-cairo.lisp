fbb73b6c7efea65ba3c457ed0ff468330042844f
Crategus <kaiser@crategus.com>
2024-02-10 14:41:20 +0100

    Add an utility function

--- test/rtest-cairo.lisp
+++ test/rtest-cairo.lisp
@@ -25,6 +25,7 @@
   (asdf:system-relative-pathname system
                                  (concatenate 'string "test/" filename)))
 
+;; Utility function for the testsuite
 (defun flatten (tree)
   (let (lst)
     (labels ((traverse (subtree)
@@ -37,4 +38,19 @@
       (traverse tree))
     (nreverse lst)))
 
-;;; --- 2023-11-5 --------------------------------------------------------------
+;; Draw function used for the tests
+(defun draw-stroke (context width height)
+  (cairo:save context)
+  ;; Clear surface
+  (cairo:set-source-rgb context 1.0 1.0 1.0)
+  (cairo:paint context)
+  ;; Example is in 1.0 x 1.0 coordinate space
+  (cairo:scale context width height)
+  ;; Drawing code goes here
+  (setf (cairo:line-width context) 0.1)
+  (cairo:set-source-rgb context 1.0 0.0 0.0)
+  (cairo:rectangle context 0.25 0.25 0.5 0.5)
+  (cairo:stroke context)
+  (cairo:restore context))
+
+;;; 2024-1-14
